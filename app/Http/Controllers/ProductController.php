c9330e5f6a0d877727cc278cca93b860d7fcfbfe
wildanhanifabdillah <wildanhanifabdillah27@gmail.com>
2024-10-08 14:56:09 +0700

    first commit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,17 +6,17 @@ use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 use App\Http\Requests\StoreProductRequest;
 use App\Http\Requests\UpdateProductRequest;
-
-class Controller extends Controller
+use App\Models\Product;
+class ProductController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index() : View
     {
-        return view('index', [
-            'products' => Product::latest()->paginate(3)
-        ]);
+        $products = Product::latest()->paginate(3);
+
+        return view('products.index', compact('products'));
     }
 
     /**
@@ -32,7 +32,7 @@ class Controller extends Controller
      */
     public function store(StoreProductRequest $request) : RedirectResponse
     {
-        Product::create($request->all());
+        ProductController::create($request->all());
         return redirect()->route('index')
                 ->withSuccess('New product is added successfully.');
     }
@@ -43,7 +43,7 @@ class Controller extends Controller
     public function show(Product $product) : View
     {
         return view('products.show', [
-            'product' => $products
+            'product' => $product
         ]);
     }
 
@@ -70,7 +70,7 @@ class Controller extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product) : RedirectResponse
+    public function destroy(Product $product) : RedirectResponse
     {
         $product->delete();
         return redirect()->route('index')
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', Controller::class);
+Route::get('/products', [ProductController::class, 'index']);
